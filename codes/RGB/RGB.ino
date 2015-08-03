2c32796df2149ff5b9c9baa67dd16ac74e5c55cd
Sophia <scastellarin95@gmail.com>
2015-08-03 19:38:03 -0400

    Some code for RGB leds

---
+++ codes/RGB/RGB.ino
@@ -0,0 +1,37 @@
+#define R_OUT 9
+#define G_OUT 10
+#define B_OUT 11
+#define R_IN 7
+#define B_IN 6
+#define G_IN 5
+
+void setup() {
+  pinMode(R_OUT, OUTPUT);
+  pinMode(G_OUT, OUTPUT);
+  pinMode(B_OUT, OUTPUT);
+  pinMode(R_IN, INPUT);
+  pinMode(G_IN, INPUT);
+  pinMode(B_IN, INPUT);
+}
+
+void loop() {
+  
+  if (digitalRead(R_IN) == HIGH){
+    digitalWrite(R_OUT, HIGH);
+  }else{
+   digitalWrite(R_OUT, LOW);
+  } 
+  
+  if (digitalRead(G_IN) == HIGH){
+    digitalWrite(G_OUT, HIGH);
+  }else{
+   digitalWrite(G_OUT, LOW);
+  } 
+  
+  if (digitalRead(B_IN) == HIGH){
+    digitalWrite(B_OUT, HIGH);
+  }else{
+   digitalWrite(B_OUT, LOW);
+  } 
+}
+
