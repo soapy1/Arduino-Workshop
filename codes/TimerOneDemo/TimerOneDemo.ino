0e0b3d8209a51a09e4e227918cd8b99b562bf6eb
Kurt Mohler <kurtamohler@gmail.com>
2015-08-07 21:27:12 -0400

    Made TimerOneDemo simpler. It does the same thing as DelayAndMillis, and demonstrates the accuracy of timers

--- codes/TimerOneDemo/TimerOneDemo.ino
+++ codes/TimerOneDemo/TimerOneDemo.ino
@@ -1,23 +1,14 @@
 #include <TimerOne.h>
 
-bool ledState = false;
-
-void toggleLed() {
-  digitalWrite(3, ledState);
-  ledState = !ledState;
-}
-
-void doSomethingElse() {
-  delay(10000); // Wait 10 seconds
+void printTime() {
+  Serial.println(millis());
 }
  
 void setup() {
-  pinMode(3, OUTPUT);
+  Serial.begin(9600);
   Timer1.initialize(1000000);// Set timer length to 1 sec
-  Timer1.attachInterrupt(toggleLed); // Print message every second
+  Timer1.attachInterrupt(printTime); // Print message every second
 }
 
-// Now you can run code in parallel down here
 void loop() {
-  doSomethingElse();
 }
